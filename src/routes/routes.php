fc975253bad0713c6709afdf09df03f23e13b31d
Joshua Kissoon <joshuakissoon@Joshuas-MacBook-Pro.local>
2019-04-30 11:39:40 -0400

    Cleaned up the Readme and Routes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,12 +2,12 @@
 
 Route::group(['prefix' => 'api', 'middleware' => 'api'], function()
 {
-    Route::post("files/upload", "Techlify\FileManager\Controllers\FilesController@upload");
+    Route::post("techlify-files/upload", "Techlify\FileManager\Controllers\FilesController@upload");
 
     /* Files Management */
-    Route::post("files", "\Techlify\FileManager\Controllers\FilesController@store");
-    Route::get("files", "\Techlify\FileManager\Controllers\FilesController@index");
-    Route::put("files/{id}", "\Techlify\FileManager\Controllers\FilesController@update");
-    Route::get("files/{id}", "\Techlify\FileManager\Controllers\FilesController@show");
-    Route::delete("files/{id}", "\Techlify\FileManager\Controllers\FilesController@destroy");
+    Route::post("techlify-files", "\Techlify\FileManager\Controllers\FilesController@store");
+    Route::get("techlify-files", "\Techlify\FileManager\Controllers\FilesController@index");
+    Route::put("techlify-files/{id}", "\Techlify\FileManager\Controllers\FilesController@update");
+    Route::get("techlify-files/{id}", "\Techlify\FileManager\Controllers\FilesController@show");
+    Route::delete("techlify-files/{id}", "\Techlify\FileManager\Controllers\FilesController@destroy");
 });
